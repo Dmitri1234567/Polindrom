80dbcd3115431dc42b48fa5521b7ae26c1ae40bd
Dima <dmitrijskurcenko3@gmail.com>
2022-09-29 11:10:14 +0300

    Polindrom
    Polindrom

---
+++ src/polindrom/Polindrom1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package polindrom;
+
+/**
+ *
+ * @author admin
+ */
+public class Polindrom1 {
+    private String s;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+    
+    public void sequreti(){
+        if(s)
+    }
+    
+}
